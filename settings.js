debf0f0d5c4cd1e6f5d5fa15e718f71df1221a6d
unknown <jaopds2004@gmail.com>
2024-11-18 17:01:25 -0300

    botscwhatsappv2

---
+++ settings.js
@@ -0,0 +1,12 @@
+// 𝗖𝗢𝗡𝗧𝗥𝗢𝗟 𝗕𝗢𝗧 
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['5544998477529'] //OWNER
+global.botname = 'Jvps_Mods_TechMod' //BOT NAME
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+//global.baileys2 = require("@adiwajshing/baileys") // NOT CHANGE
+global.creator = "Jvps_Mods_TechMod" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Jvps_Mods" //OPSIONAL
+global.wm = "Jvps_Mods_TechMod" // OPSIONAL
+global.idCH = "120363321780343299@newsletter" // OPSIONAL
